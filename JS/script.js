c504574c2e5149bca7ec741ce7d6d3b15634c80f
Filip Stangricki <stangricki@gmail.com>
2017-11-21 18:26:29 +0100

    First commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+var list = document.getElementById('list')
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function() {
+	var element = document.createElement('li');
+	element.innerHTML = 'item ' + list.getElementsByTagName('li').length;
+	list.appendChild(element);
+});
+
